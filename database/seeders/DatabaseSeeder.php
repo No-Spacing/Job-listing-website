5fb80dcf93e6beed9b327c5c0e40a804a75c14ba
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-12-10 16:17:46 +0800

    Added Database on Job List

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,53 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Job::create([
+            'name' => 'Converge ICT Solutions INC.',
+            'description' => "Converge provides the absolute premium fiber 
+            internet experience for home, businesses and 
+            in the corporate world, we are the brand of choice.",
+            'requirements' => "WE'RE HIRING!!
+                        SALES AGENT
+                        QUALIFICATIONS:
+                        - Must have sales related working
+                        - Can work in field independently
+                        - Can negotiate to Company's Top Management
+                        EMPLOYMENT DETAILS
+                        Unlimited Income
+                        FREE Training (Online)
+                        Life Insurance
+                        REQUIREMENTS:
+                        Resume/Bio-data
+                        (1) Valid ID
+                        Brgy. Clearance/Police or NBI Clearance (1 only).",
+            'image' => 'images/converge_cover.jpg',
+        ]);
+
+        \App\Models\Job::create([
+            'name' => 'Aeon Credit Service',
+            'description' => "Aeon Philippines is the best online lending company in the Philippines. 
+            Minimum requirements needed and low interest rates.",
+            'requirements' => "WE'RE HIRING‼
+            We are looking for professional and passionate individuals to join our team as:
+            SALES PROMOTER
+            for AEON CREDIT SERVICES INC.
+            Qualifications:
+            With at least 6 monts to 1 year Sales Experience
+            High School Graduate / College Level
+            NO Pending Itime
+            NO Visible Tattoo
+            Employment Details:
+            610/day (Metro Manila) | 550/day (Province)
+            Paid Training (5 days)
+            Laptop & Pocket Wifi Provided
+            Life Insurance
+            You may send your resume at:
+            Email: recruitment.trismanpower@gmail.com
+            Subject: (Input here your preferred POSITION - AREA)
+            0910-717-1958 or message us here.",
+            'image' => 'images/aeon.png',
+            
+        ]);
     }
 }
