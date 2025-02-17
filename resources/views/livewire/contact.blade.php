2490dd8fb5f154145bf92e3ef40564ddd5b794d3
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-02-17 20:39:08 +0800

    replace contact details

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -1,39 +1,32 @@
 <div>
-    <div class="container mt-3">
-        <div class="red-bar">
-            <h1 class="text-center text-white">Contact Us</h1>
-        </div>    
+    <div class="red-bar">
+        <h1 class="text-center text-white">Contact Us</h1>
+    </div>    
 
-        <div class="d-flex justify-content-center mt-5 mb-3">
-            <img class="" src="{{ Storage::url('images/logo2.png') }}" style="height: auto; width: 100px"/>
-        </div>
-        <h6 class="text-center">TRI-S MANPOWER <br> MANAGEMENT SERVICES</h6>
-        <div class="mt-5">
-            <div class="container d-flex justify-content-between">
-                <div class="text-center">
-                    <h3>Address</h3>
-                    <span class="fw-bold">Main Office</span><br>
-                    <span>
-                        Suite 407 Columbian Building<br>
-                        160 West Avenue<br>
-                        Brgy. Philam, Quezon City<br>
-                        (02)397-9201<br>
-                    </span><br>
-                </div>
-                <div class="text-center">
-                    <h3>Contact Person</h3>
-                    <span class="fw-semibold">MA. AINI E. SALVADORA</span><br>
-                    <span>
-                        Operations Manager<br>
-                        0955-966-5273 | 0989-794-1769
-                    </span>
-                </div>
-                <div class="text-center">
-                    <h3>Email</h3>
-                    <span>recruitment.trismanpower@gmail.com</span><br>
-                    <span>trismmshrd.asst@gmail.com</span>
-                 </div>
+    <div class="d-flex justify-content-center mt-5 mb-3">
+        <img class="" src="images/job-icon.png" style="height: auto; width: 100px"/>
+    </div>
+    <h3 class="text-center">Job Finding Service</h3>
+    <div class="mt-5">
+        <div class="container d-flex justify-content-between">
+            <div class="text-center">
+                <h3>Address</h3>
+                <span class="fw-bold">Your Office</span><br>
+                <span>
+                    Your Office Address
+                </span><br>
             </div>
+            <div class="text-center">
+                <h3>Contact Person</h3>
+                <span class="fw-semibold">Your Name</span><br>
+                <span>
+                    Your Contact Number
+                </span>
+            </div>
+            <div class="text-center">
+                <h3>Your Email</h3>
+                <span>yourcompany@email.com</span><br>
+                </div>
         </div>
     </div>
 </div>
