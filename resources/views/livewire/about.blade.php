3d8f196d56b063d7b78d0ec68c0ad977a5e445c7
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-02-17 20:39:19 +0800

    replace about page

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -1,16 +1,14 @@
 <div>
+    <div class="red-bar">
+        <h1 class="text-center text-white">About Us</h1>
+    </div>
+    
+    <div class="d-flex justify-content-center">
+        <img src="images/job-search-cover.jpeg" style="height: auto; width: 550px;">
+    </div>
+    
     <div class="container mt-3">
-        <div class="red-bar">
-            <h1 class="text-center text-white">About Us</h1>
-        </div>
-        
-        <div class="d-flex justify-content-center">
-            <img src="{{ Storage::url('images/community.jpg') }}" style="height: auto; width: 550px;">
-        </div>
-        
-        <div class="mt-3"></div>
-            <span class="mms">TRI - S MANPOWER MANAGEMENT SERVICES</span>
-            <span class="details"> is dedicated to bridging the gap between talent and opportunity, offering a comprehensive suite of workforce solutions designed to meet the ever-evolving demands of the modern business landscape. As a client-centric organization, Tri-S understands the significance of human capital in driving success, and thus, it specializes in providing top-notch staffing services to businesses of all sizes.</span>
-        </div>
+        <span class="mms">Job Finding Website</span>
+        <span class="details"> is dedicated to bridging the gap between talent and opportunity, offering a comprehensive suite of workforce solutions designed to meet the ever-evolving demands of the modern business landscape. As a client-centric organization, Tri-S understands the significance of human capital in driving success, and thus, it specializes in providing top-notch staffing services to businesses of all sizes.</span>
     </div>
 </div>
